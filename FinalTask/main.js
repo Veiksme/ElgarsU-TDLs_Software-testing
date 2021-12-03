ead8b61ea7a12bccdd6c438b39d55ee089178f05
Elgars <elgars.upitis@gmail.com>
2021-12-03 18:27:50 +0200

    Implement dummy function for case 'red'

--- FinalTask/main.js
+++ FinalTask/main.js
@@ -25,28 +25,23 @@ rl.question('Choose blue or red pill:\n', userInput => {
             });
             break;
         case 'red':
-            console.log('case2:');
-            rl.close();
+            console.log('_red_Please enter two numbers to be compared (seperated by enter):')
+            rl.question('_red_Enter first number\n', function(num1){
+                rl.question('_red_Enter second number\n', function(num2){
+                    let result = biggerNumber(parseInt(num1), parseInt(num2));
+                    console.log('_red_Biggest number is: ', result);
+                        fs.writeFile('./results.txt', `_red_biggerNumber(${parseInt(num1)}, ${parseInt(num2)}) = ${result}`, () => {
+                        console.log('_red_Result is written in results.txt');
+                    });
+                    rl.close();
+                });
+            });
             break;
         default:
             console.log('Incorrect input. Terminating!');
             rl.close();
     }
-    //rl.close();
 });
-
-//Function for comparing two numbers and printing biggest of them to console and results.txt file
-//console.log('Please enter two numbers to be compared (seperated by enter):')
-//rl.question('Enter first number\n', function(num1){
-//    rl.question('Enter second number\n', function(num2){
-//        let result = biggerNumber(parseInt(num1), parseInt(num2));
-//        console.log('Biggest number is: ', result);
-//            fs.writeFile('./results.txt', `biggerNumber(${parseInt(num1)}, ${parseInt(num2)}) = ${result}`, () => {
-//            console.log('Result is written in results.txt');
-//        });
-//        rl.close();
-//    });
-//});
 
 //function numberSquares test in console
 //let n = 3;
