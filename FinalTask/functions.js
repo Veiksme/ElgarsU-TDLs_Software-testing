e9794ac23698f4f5d98059dbb0a0abdf21f0155c
Elgars <elgars.upitis@gmail.com>
2021-12-03 11:40:31 +0200

    Create and test biggerNumber function

---
+++ FinalTask/functions.js
@@ -0,0 +1,32 @@
+
+//Function to return biggest number of two
+function biggerNumber(a, b) {
+    let biggestNumber;
+    if (a > b) {
+        biggestNumber = a
+        return biggestNumber;
+    } else {
+        biggestNumber = b
+        return biggestNumber;
+    }
+}
+
+//function biggerNumber test in console
+
+console.log('Function returns biggest number of two: ', biggerNumber(50, 9));
+
+//Function to calculate number of individual squares in n * n grid
+
+function numberSquares(n) {
+    let squares;
+    if (n = 0) {
+        squares = 'You entered 0, square count is 0';
+        return squares;
+//    } else {
+//        let squareItterator = n;
+//        do {
+//            squares = 
+    }
+    
+    }
+//}
