20261f089a91d25b015d652682c4f8b426f2b3e8
Elgars <elgars.upitis@gmail.com>
2021-12-03 17:44:32 +0200

    Ask user input and write to file for biggerNumber

--- FinalTask/main.js
+++ FinalTask/main.js
@@ -1,10 +1,25 @@
 const [biggerNumber, numberSquares] = require('./functions.js');
+const fs = require('fs');
+const readLine = require('readline');
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter first number\n', function(num1){
+    rl.question('Enter second number\n', function(num2){
+        let result = biggerNumber(parseInt(num1), parseInt(num2));
+        console.log('Biggest number is: ', result);
+            fs.writeFile('./results.txt', `biggerNumber(${parseInt(num1)}, ${parseInt(num2)}) = ${result}`, () => {
+            console.log('Result is written in results.txt');
+        });
+        rl.close();
+    });
+});
+
 
 //function biggerNumber test in console
-let a = 5;
-let b = 10;
-console.log('Biggest number is: ', biggerNumber(a, b));
-
+//console.log('Biggest number is: ', biggerNumber(a, b));
 //function numberSquares test in console
-let n = 3;
-console.log('Total individual squares: ', numberSquares(n));
+//let n = 3;
+//console.log('Total individual squares: ', numberSquares(n));
