0c5fcb7e71654079a4dd555107ecb460da95ac38
Elgars Upitis <elgars.upitis@gmail.com>
2021-12-08 11:09:00 +0200

    Styling

--- CypressTesting/cypress/integration/day2/login.spec.js
+++ CypressTesting/cypress/integration/day2/login.spec.js
@@ -1,13 +1,11 @@
-import { LoginPage } from "../../pageObjects/pages/LoginPage"
-import { ProductsPage } from "../../pageObjects/pages/ProductsPage"
+import { LoginPage } from "../../pageObjects/pages/LoginPage";
+import { ProductsPage } from "../../pageObjects/pages/ProductsPage";
 
-let loginUsername = "standard_user"
-
+let loginUsername = "standard_user";
 
 describe("Login test cases", () => {
-    it("Log in with standard user", () => {
-        LoginPage.loginWithUI(loginUsername)
-        ProductsPage.verifyUserLoggedIn()
-    })
-})
-
+  it("Log in with standard user", () => {
+    LoginPage.loginWithUI(loginUsername);
+    ProductsPage.verifyUserLoggedIn();
+  });
+});
--- CypressTesting/cypress/pageObjects/pages/CartPage.js
+++ CypressTesting/cypress/pageObjects/pages/CartPage.js
@@ -1,12 +1,13 @@
+/// <reference types="Cypress" />
+
 import { BasePage } from "../BasePage";
 
-const ALL_CART_ITEM_NAMES = ".inventory_item_name"
+const ALL_CART_ITEM_NAMES = ".inventory_item_name";
 
 export class CartPage extends BasePage {
-
-    static verifyAddedItem(){
-        cy.get("@addedItem").then((addedItem) =>{
-            cy.get(ALL_CART_ITEM_NAMES).should("have.text", addedItem)
-        })
-    } 
+  static verifyAddedItem() {
+    cy.get("@addedItem").then((addedItem) => {
+      cy.get(ALL_CART_ITEM_NAMES).should("have.text", addedItem);
+    });
+  }
 }
